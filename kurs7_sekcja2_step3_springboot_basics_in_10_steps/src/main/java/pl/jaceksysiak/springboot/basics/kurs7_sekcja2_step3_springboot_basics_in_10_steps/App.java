e35bfc7a88a2bbd204ec6470ecf3b6a1730050b3
Jacek <Jacek@BERLIN>
2020-04-26 09:54:37 +0200

    cc

---
+++ kurs7_sekcja2_step3_springboot_basics_in_10_steps/src/main/java/pl/jaceksysiak/springboot/basics/kurs7_sekcja2_step3_springboot_basics_in_10_steps/App.java
@@ -0,0 +1,13 @@
+package pl.jaceksysiak.springboot.basics.kurs7_sekcja2_step3_springboot_basics_in_10_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
